6dc1a9a08018510857aa4468e3dea6585b6f1c0d
Geovane Mayan <126290680+Mayanzx@users.noreply.github.com>
2026-02-06 00:27:57 +0000

    database.js mais robusto a erros

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseVersionString = databaseVersion.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsString = databaseMaxConnections.rows[0].max_connections;
+
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const openedConnectionsString = databaseOpenedConnections.rows[0].count;
+  console.log("Opened connections query result:", openedConnectionsString);
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionString,
+        max_connections: parseInt(maxConnectionsString),
+        opened_connections: openedConnectionsString,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status returns status 200 and correct body", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  const data = await response.json();
+  expect(data.updated_at).toBeDefined();
+
+  const expectedUpdatedAt = new Date(data.updated_at).toISOString();
+  expect(data.updated_at).toEqual(expectedUpdatedAt);
+
+  expect(data.dependencies.database.version).toEqual("16.0");
+  expect(data.dependencies.database.max_connections).toEqual(100);
+  expect(data.dependencies.database.opened_connections).toBeGreaterThanOrEqual(
+    1,
+  );
 });
