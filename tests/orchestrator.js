a110159cde110e4934a045a90cc3ad24770fded5
Geovane Mayan <126290680+Mayanzx@users.noreply.github.com>
2026-02-26 22:56:51 +0000

    adiciona função para aguardar a prontidão de todos os serviços

---
+++ tests/orchestrator.js
@@ -0,0 +1,25 @@
+const retry = require("async-retry");
+const { getMaxAge } = require("next/dist/server/image-optimizer");
+
+async function waitForAllServices() {
+  await waitForWebServer();
+}
+
+async function waitForWebServer() {
+  return (
+    retry(fetchStatusPage),
+    {
+      retries: 100,
+      maxTimeout: 1000,
+    }
+  );
+
+  async function fetchStatusPage() {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const responseBody = await response.json();
+  }
+}
+
+module.exports = {
+  waitForAllServices,
+};
